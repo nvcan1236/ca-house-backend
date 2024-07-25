faf5b3d1313cee30917b66f1138292db92973351
NguyenVanCanh <ngcanh1236@gmail.com>
2024-07-25 19:02:26 +0700

    init project

---
+++ ca-house/src/test/java/com/nvc/ca_house/CaHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvc.ca_house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
